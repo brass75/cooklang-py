7769cb9d5ef1f74d9926d5dbbbe6328a897cf772
its-Sohan <stathammurphy141@gmail.com>
2026-07-06 09:13:50 +0600

    refactor: use divmod in WholeFraction.__str__
    Replace manual numerator//denominator and abs(self) - whole with
    Python's built-in divmod for cleaner, more idiomatic code.
    
    Signed-off-by: its-Sohan <stathammurphy141@gmail.com>

--- src/cooklang_py/utils.py
+++ src/cooklang_py/utils.py
@@ -5,8 +5,8 @@ from fractions import Fraction
 
 class WholeFraction(Fraction):
     def __str__(self):
-        whole = abs(self.numerator) // self.denominator
-        part = abs(self) - whole
+        whole, remainder = divmod(abs(self.numerator), self.denominator)
+        part = Fraction(remainder, self.denominator)
         sign = '-' if self < 0 else ''
         if whole and part:
             return f'{sign}{whole} {part}'
